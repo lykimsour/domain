f6fa696bf2fd46dba0d46749751214842e59ad37
kimneak <chi.kimneak@cidc.com.kh>
2015-10-19 16:43:07 +0700

    report usertoshoplog and merchantgameview

---
+++ app/UserToMerchantgameviewDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserToMerchantGameViewDetail extends Model
+{
+    protected $table = 'transfer_user2merchant_gameview_d';
+}
