d98130ba2194fe305a86b807557c5ac4f1f46947
kimneak <chi.kimneak@cidc.com.kh>
2015-10-21 15:00:00 +0700

    new model UserToMerchantIluckyDetail & TlJ

---
+++ app/UserToMerchantIluckyDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserToMerchantIluckyDetail extends Model
+{
+	protected $table = 'transfer_user2merchant_ilucky_d';
+}
---
+++ app/UserToMerchantTouslesjoursDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserToMerchantTouslesjoursDetail extends Model
+{
+    protected $table = 'transfer_user2merchant_touslesjours_d';
+}
